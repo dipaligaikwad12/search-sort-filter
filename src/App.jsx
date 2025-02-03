b9700a29162c7e7c03ec1d84b85a390cf593eb31
dipaligaikwad12 <gdipali070@gmail.com>
2025-02-03 19:40:53 +0530

    search app created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className=''>
+    <h1 className='text-center text-blue-500 text-4xl'>Search, Sort, Filter</h1>
+    </div>
+  )
+}
+
+export default App
